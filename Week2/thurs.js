64cd84a5429b4de7eea91c553bbac7a662935302
Joe Baney <joebaney@outlook.com>
2026-02-26 12:30:24 -0600

    finished a couple of JS challenges today on my own

---
+++ Week2/thurs.js
@@ -0,0 +1,48 @@
+// Challenge 1. Balanced Brackets - Write a function that returns true or false if all brackets are properly matched and closed
+const isBalanced = (str) => {
+  let stack = [];
+  let pairs = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  let openingBrackets = "({[";
+  let closingBrackets = ")}]";
+
+  for (let i = 0; i < str.length; i++) {
+    if (openingBrackets.includes(str[i])) {
+      stack.push(str[i]);
+    } else if (closingBrackets.includes(str[i])) {
+      let popped = stack.pop();
+      if (pairs[popped] !== str[i]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+// Challenge 2. Write a function that compares two strings to see if they are anagrams
+const isAnagram = (str1, str2) => {
+  let cleanStr1 = str1
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .split("")
+    .sort()
+    .join("");
+  let cleanStr2 = str2
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .split("")
+    .sort()
+    .join("");
+
+  return cleanStr1 === cleanStr2;
+};
+
+// Challenge 3. Return the element that appears most frequently in the array, if there's a tie, return the first one
+const mostFrequent = (arr) => {};
+
+console.log(mostFrequent([1, 3, 1, 3, 2, 1])); // 1
+console.log(mostFrequent(["a", "b", "a", "b"])); // "a"
